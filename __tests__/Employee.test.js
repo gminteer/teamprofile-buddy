3c525523d5361bb53a9d37d963f53ca603b3304b
gminteer <gminteer@gmail.com>
2020-08-04 16:28:00 -0400

    refactored tests, added Manager

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,17 +1,16 @@
 const {validate: uuidValidate} = require('uuid');
 const Employee = require('../lib/Employee');
 
-test('creates an employee', () => {
+test('creates an employee, verifies employee has a company', () => {
   const employee = new Employee('John Doe');
 
-  expect(employee).toEqual(expect.any(Employee));
-  expect(employee.getName()).toEqual('John Doe');
   expect(employee.company).toEqual(expect.objectContaining({name: expect.any(String), domain: expect.any(String)}));
 });
 
 test("checks employee's getter functions", () => {
   const employee = new Employee('John Doe');
 
+  expect(employee.getName()).toEqual('John Doe');
   expect(uuidValidate(employee.getId())).toEqual(true);
   expect(employee.getEmail()).toEqual(`${employee.getName().replace(/\s/g, '.')}@${employee.company.domain}`);
   expect(employee.getRole()).toEqual('Employee');
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,17 @@
+const {validate: uuidValidate} = require('uuid');
 const Manager = require('../lib/Manager');
 
-test('creates a manager', () => {
-  const manager = new Manager('John Doe', {officeNumber: '+15555551212'});
+test('creates a manager, verifies the manager has a company', () => {
+  const manager = new Manager('John Doe');
 
   expect(manager.company).toEqual(expect.objectContaining({name: expect.any(String), domain: expect.any(String)}));
+});
+
+test("checks manager's getter functions", () => {
+  const manager = new Manager('John Doe');
+
+  expect(manager.getName()).toEqual('John Doe');
+  expect(uuidValidate(manager.getId())).toEqual(true);
+  expect(manager.getEmail()).toEqual(`${manager.getName().replace(/\s/g, '.')}@${manager.company.domain}`);
+  expect(manager.getRole()).toEqual('Manager');
 });
