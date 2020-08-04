551bd22ef910c16b636e38706147f1e14b008a2c
gminteer <gminteer@gmail.com>
2020-08-04 15:38:47 -0400

    Employee class, updated tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,11 @@
+const {validate: uuidValidate} = require('uuid');
 const Employee = require('../lib/Employee');
 
 test('creates an employee', () => {
   const employee = new Employee('John Doe');
 
   expect(employee.getName()).toEqual('John Doe');
-  expect(employee.getId()).toEqual(expect.any(String));
-  expect(employee.getEmail()).toEqual(expect.stringContaining('@'));
+  expect(uuidValidate(employee.getId())).toEqual(true);
+  expect(employee.getEmail()).toEqual(`${employee.getName().replace(/\s/g, '.')}@${employee.company_domain}`);
   expect(employee.getRole()).toEqual('Employee');
 });
